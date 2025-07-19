f132eb36bc8f26b47bc5912af5bf4d6d1dfe31c2
SholkeInterprise <anujsholke@gmail.com>
2025-07-19 16:07:10 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Basic HTML Page</title>
+</head>
+<body>
+  <div class='di'> </div>
+    <h1>Hello, World!</h1>
+    <p>This is a basic HTML page.</p>
+</body>
+</html>
